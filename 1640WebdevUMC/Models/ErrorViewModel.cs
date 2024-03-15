ec430e5755070c4a459e48c23698e365b2bd3eb0
SHIELD MALER <simonwill@outlook.com.vn>
2024-03-16 00:53:12 +0700

    Add project files.

---
+++ 1640WebdevUMC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace _1640WebDevUMC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
