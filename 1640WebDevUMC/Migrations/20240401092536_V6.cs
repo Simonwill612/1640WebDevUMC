912cff8ed646d97fafd939dd940d8393b0447983
SHIELD MALER <simonwill@outlook.com.vn>
2024-04-01 16:26:19 +0700

    fixing Viewupload file v2

---
+++ 1640WebDevUMC/Migrations/20240401092536_V6.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace _1640WebDevUMC.Migrations
+{
+    /// <inheritdoc />
+    public partial class V6 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Files",
+                table: "Files");
+
+            migrationBuilder.DropColumn(
+                name: "Id",
+                table: "Files");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ContributionID",
+                table: "Files",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Files",
+                table: "Files",
+                column: "ContributionID");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Files",
+                table: "Files");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ContributionID",
+                table: "Files",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Id",
+                table: "Files",
+                type: "int",
+                nullable: false,
+                defaultValue: 0)
+                .Annotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Files",
+                table: "Files",
+                column: "Id");
+        }
+    }
+}
--- 1640WebdevUMC/Models/File.cs
+++ 1640WebdevUMC/Models/File.cs
@@ -5,12 +5,11 @@ namespace _1640WebDevUMC.Models
     public class File
     {
         [Key]
-        public int Id { get; set; }
+        public string ContributionID { get; set; } // This property associates the file with a contribution
 
         public string FileName { get; set; }
 
         public string FilePath { get; set; }
 
-        public string ContributionID { get; set; } // This property associates the file with a contribution
     }
 }
