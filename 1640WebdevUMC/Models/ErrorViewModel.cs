e489fe1c6267922bfe1026d05fb1e4c375db8554
Simonwill612 <trungtinlethai@gmail.com>
2024-04-28 23:42:01 +0700

    Delete 1640WebdevUMC directory

--- 1640WebdevUMC/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace _1640WebDevUMC.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
